6705b15d583f94d080604f1885e991f21efdc2f9
yixin0711 <xinyi@gatech.edu>
2020-07-01 09:08:48 -0700

    modified problem 1

--- project3/problem_1.py
+++ project3/problem_1.py
@@ -26,21 +26,26 @@ def sqrt(number):
     if number == 0 or number == 1:
         return number
     
-    start = 1
+    start = 0
     end = number
+
+    return sqrt_recursive(start, end, number)
+
+def sqrt_recursive(start, end, number):
     
-    while start <= end:
-        mid = (start + end) // 2
+    if start <= end:
         
+        mid = (start + end)//2
         if mid*mid == number:
             return mid
         elif mid*mid < number:
-            start = mid + 1
-            ans = mid
+            if (mid+1)*(mid+1) > number:
+                return mid
+            else:
+                return sqrt_recursive(mid+1, end, number)
         else:
-            end = mid - 1
-    
-    return ans
+            return sqrt_recursive(start, mid-1, number)
+            
 
 #Test edge cases
 sqrt(-1)        #should print "Invalid Input, should be positive number"
